c95498805428a41a76989ed72c7461ff9b3d037a
austentyk <austen.tyk@gmail.com>
2023-11-21 14:31:55 +0800

    2nd for jdk 17

--- d11lecture/src/main/java/sg/edu/nus/iss/d11lecture/D11lectureApplication.java
+++ d11lecture/src/main/java/sg/edu/nus/iss/d11lecture/D11lectureApplication.java
@@ -1,5 +1,12 @@
 package sg.edu.nus.iss.d11lecture;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import javax.swing.Spring;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +14,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class D11lectureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(D11lectureApplication.class, args);
+
+		//SpringApplication.run(D11lectureApplication.class, args);
+
+		SpringApplication app = new SpringApplication(D11lectureApplication.class);
+
+		String port = "8080";
+		ApplicationArguments argOptions = new DefaultApplicationArguments(args); //detects arguments
+
+		if(argOptions.containsOption("port")) {
+			port = argOptions.getOptionValues("port").get(0); //use the argument as port
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port)); // sets the argument as port
+
+		app.run(args);
+
+		System.out.println("My first springboot application started...");
 	}
 
 }
